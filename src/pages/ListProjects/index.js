c4bd3b0a837ab4e711fe6079c5882d83a535dfa9
JuliandroR <juliandro.rocha@gmail.com>
2020-11-21 00:00:31 -0400

    Implementa listagem de projetos na Aba Projetos

--- src/pages/ListProjects/index.js
+++ src/pages/ListProjects/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PageDefault, SpaceBetween, SafeArea } from "../../components/Views";
 import HeaderMenu from "../../components/HeaderMenu";
-import NumberInfo from "../../components/NumberInfo";
-import { ScrollView, View, Image, Text, TouchableOpacity } from "react-native";
+import {
+  ScrollView,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 import { styles } from "./styles";
@@ -10,51 +17,78 @@ import TitleApp from "../../components/TitleApp";
 import ContainerInfo from "../../components/ContainerInfo";
 
 const background_card = require("../../assets/img/project_card_background.png");
+import firebase from "firebase";
 
 const ListProjects = () => {
+  useEffect(() => {
+    (async () => {
+      await getDataProjects();
+    })();
+  });
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  async function getDataProjects() {
+    let dataProjects = [];
+    let data = [];
+
+    await firebase
+      .database()
+      .ref("/tbl_projetos")
+      .once("value", async (snapshot) => {
+        snapshot.forEach((child) => {
+          dataProjects.push(child);
+        });
+      });
+    dataProjects.forEach(function (project, index) {
+      data.push({
+        ...project.val(),
+        key: `${index}`,
+      });
+    });
+    setDataProjects(data);
+    setLoading(false);
+    setVisible(true);
+  }
+
+  const [loading, setLoading] = useState(true);
+  const [visible, setVisible] = useState(false);
+  const [dataProjects, setDataProjects] = useState(null);
+
   return (
     <PageDefault style={{ paddingTop: 75 }}>
       <HeaderMenu viewBack={true} />
       <ContainerInfo />
       <TitleApp title="Lista de Projetos" />
       <SafeArea>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.container}>
-            <Image source={background_card} style={styles.image} />
-            <Text style={styles.titleProject}>Lorem Ipsum</Text>
-            <SpaceBetween>
-              <Text style={styles.description}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-                nostrum quos repudiandae, possimus necessitatibus animi.
-              </Text>
-              <TouchableOpacity onPress={() => {}}>
-                <Entypo
-                  name="arrow-with-circle-right"
-                  size={40}
-                  color="#885500"
-                />
-              </TouchableOpacity>
-            </SpaceBetween>
-          </View>
-
-          <View style={styles.container}>
-            <Image source={background_card} style={styles.image} />
-            <Text style={styles.titleProject}>Lorem Ipsum</Text>
-            <SpaceBetween>
-              <Text style={styles.description}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-                nostrum quos repudiandae, possimus necessitatibus animi.
-              </Text>
-              <TouchableOpacity onPress={() => {}}>
-                <Entypo
-                  name="arrow-with-circle-right"
-                  size={40}
-                  color="#885500"
-                />
-              </TouchableOpacity>
-            </SpaceBetween>
-          </View>
-        </ScrollView>
+        {visible && (
+          <FlatList
+            data={dataProjects}
+            keyExtractor={(project) => project.key}
+            renderItem={(project) => (
+              <View style={styles.container}>
+                <Image source={background_card} style={styles.image} />
+                <Text style={styles.titleProject}>
+                  {project.item.nomeprojeto}
+                </Text>
+                <SpaceBetween>
+                  <Text style={styles.description}>
+                    {project.item.resumoprojeto}
+                  </Text>
+                  <TouchableOpacity onPress={() => {}}>
+                    <Entypo
+                      name="arrow-with-circle-right"
+                      size={40}
+                      color="#885500"
+                    />
+                  </TouchableOpacity>
+                </SpaceBetween>
+              </View>
+            )}
+          />
+        )}
       </SafeArea>
     </PageDefault>
   );
